14c40b5affaadda15ee6ba73d88c0c1b13e47074
Elina Hovakimyan <elina.hovakimyan@firstvet.com>
2022-10-27 12:53:58 +0400

    Remove class component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 import Title from '../../components/Title';
-import TitleClass from '../../components/Title/classComponent';
 
 const Home = () => {
-    const [title, setTitle] = useState('My First Component');
-
-    useEffect(() => {
-        setTimeout(() => {
-            setTitle('Updated prop text');
-        }, 3000);
-    }, []);
-    console.log('title :>> ', title);
-
     return (
         <SafeAreaView>
             <View>
                 <Title />
-
-                <TitleClass text='Class Component' />
             </View>
         </SafeAreaView>
     );
