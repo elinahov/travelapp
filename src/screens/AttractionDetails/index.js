6cfe794a511e09a70a82ece1b041313bff5ffadf
Elina Hovakimyan <elina.hovakimyan@firstvet.com>
2022-10-31 19:04:38 +0400

    Create details page image banner

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { Image, ImageBackground, Pressable, SafeAreaView, Text, View } from 'react-native';
 import styles from './styles';
 
 const AttractionDetails = ({ navigation, route }) => {
     const { item } = route?.params || {};
+    const mainImage = item?.images?.length ? item?.images[0] : null;
+    const slicedImages = item?.images?.length ? item?.images?.slice(0, 5) : [];
+    const diffImages = item?.images?.length - slicedImages?.length;
 
     const onBack = () => {
         navigation.goBack();
     };
 
+    const onGalleryNavigate = () => {
+        navigation.navigate('Gallery', { images: item?.images });
+    };
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text onPress={onBack} style={{ margin: 32 }}>BACK</Text>
+            <ImageBackground
+                style={styles.mainImage}
+                imageStyle={{ borderRadius: 20 }}
+                source={{ uri: mainImage }}
+            >
+                <View style={styles.header}>
+                    <Pressable onPress={onBack} hitSlop={8}>
+                        <Image style={styles.icon} source={require('../../assets/back.png')} />
+                    </Pressable>
+                    <Pressable hitSlop={8}>
+                        <Image style={styles.icon} source={require('../../assets/share.png')} />
+                    </Pressable>
+                </View>
+
+                <Pressable onPress={onGalleryNavigate} style={styles.footer}>
+                    {slicedImages?.map((image, index) => (
+                        <View key={image}>
+                            <Image source={{ uri: image }} style={styles.miniImage} />
+                            {diffImages > 0 && index === slicedImages?.length - 1 ? (
+                                <View style={styles.moreImagesContainer}>
+                                    <Text style={styles.moreImages}>{`+${diffImages}`}</Text>
+                                </View>
+                            ) : null}
+                        </View>
+                    ))}
+                </Pressable>
+            </ImageBackground>
             <Text>{item?.name}</Text>
         </SafeAreaView>
     );
---
+++ src/screens/AttractionDetails/styles.js
@@ -0,0 +1,61 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 32,
+    },
+    mainImage: {
+        width: '100%',
+        height: height / 2,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    header: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    icon: {
+        width: 36,
+        height: 36,
+        margin: 16,
+    },
+    footer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15,
+        backgroundColor: 'rgba(256, 256, 256, 0.35)',
+        margin: 16,
+        paddingHorizontal: 8,
+    },
+    miniImage: {
+        width: 40,
+        height: 40,
+        marginHorizontal: 4,
+        marginVertical: 8,
+        borderRadius: 10,
+    },
+    moreImages: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    moreImagesContainer: {
+        position: 'absolute',
+        backgroundColor: 'rgba(0,0,0,0.38)',
+        width: 40,
+        height: 40,
+        top: 8,
+        left: 4,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+export default styles;
