b1e59daf8e603e6143c88de4f4901cc6433a76ed
Elina Hovakimyan <elina.hovakimyan@firstvet.com>
2022-11-07 11:36:17 +0400

    Share integration & Base64

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image, ImageBackground, Pressable, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import Share from 'react-native-share';
+import ImgToBase64 from 'react-native-image-base64';
 import InfoCard from '../../components/InfoCard';
 import Title from '../../components/Title';
 import styles from './styles';
@@ -27,6 +29,29 @@ ${item?.opening_time} - ${item?.closing_time}`;
         navigation.navigate('Gallery', { images: item?.images });
     };
 
+    const onShare = async () => {
+        try {
+            const imageWithoutParams = mainImage?.split('?')[0];
+            const imageParts = imageWithoutParams?.split('.');
+            const imageExtension = imageParts[imageParts?.length - 1];
+            const base64Image = await ImgToBase64.getBase64String(mainImage);
+
+            Share.open({
+                title: item?.name,
+                message: 'Hey, I wanted to share with you this amazing attraction',
+                url: `data:image/${imageExtension || 'jpg'};base64,${base64Image}`
+            })
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((err) => {
+                    err && console.log(err);
+                });
+        } catch (e) {
+            console.log('sharing error :>> ', e);
+        }
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
@@ -39,7 +64,7 @@ ${item?.opening_time} - ${item?.closing_time}`;
                         <Pressable onPress={onBack} hitSlop={8}>
                             <Image style={styles.icon} source={require('../../assets/back.png')} />
                         </Pressable>
-                        <Pressable hitSlop={8}>
+                        <Pressable onPress={onShare} hitSlop={8}>
                             <Image style={styles.icon} source={require('../../assets/share.png')} />
                         </Pressable>
                     </View>
