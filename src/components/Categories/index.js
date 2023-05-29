17065deec9784cfda3f52b7eb2bde9190c4c470d
worldpeacez0991 <worldpeacez0991@gmail.com>
2023-05-29 15:50:41 +0800

    Rewrite logic to remove excess ifs for margins

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -9,13 +9,13 @@ const Categories = ({ categories, selectedCategory, onCategoryPress }) => {
             data={categories}
             keyExtractor={item => String(item)}
             showsHorizontalScrollIndicator={false}
-            renderItem={({ item, index }) => {
+            renderItem={({ item }) => {
                 const selected = selectedCategory === item;
 
                 return (
                     <TouchableOpacity
                         onPress={() => onCategoryPress(item)}
-                        style={[styles.itemContainer, selected ? styles.selectedItemContainer : {}, index === 0 ? { marginLeft: 32 } : {}]}
+                        style={[styles.itemContainer, selected ? styles.selectedItemContainer : {}]}
                     >
                         <Text style={[styles.item, selected ? styles.selectedItem : {}]}>{item}</Text>
                     </TouchableOpacity>
